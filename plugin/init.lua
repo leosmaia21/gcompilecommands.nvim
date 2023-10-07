c4e46c90090dc5b472fb5cb1ec8ed9b7fe5b810d
leosmaia21 <leosmaia21@gmail.com>
2023-10-07 22:29:01 +0100

    name changed

--- plugin/init.lua
+++ plugin/init.lua
@@ -3,7 +3,7 @@
 local function generateCompileCommands()
 	vim.cmd('silent! !(make fclean)')
 	local cmd = "(make 2>&1 -wn | egrep 'directory|gcc|clang|clang\\+\\+|g\\+\\+.*' > /tmp/compile_commandsNEOVIM.json)"
-	vim.cmd("!" .. cmd)
+	vim.cmd("silent! !" .. cmd)
 	if vim.v.shell_error == 0 then
 		local f = io.open("/tmp/compile_commandsNEOVIM.json", "r")
 		if f == nil then
@@ -11,12 +11,11 @@ local function generateCompileCommands()
 			return
 		end
 		local str = f:read("*a")
-		local file = io.open(vim.fn.getcwd() .. "/compile_commands.json", "w")
+		local current_dir = vim.fn.getcwd()
+		local file = io.open(current_dir .. "/compile_commands.json", "w")
 		file:write("[\n")
 		for line in str:gmatch("[^\r\n]+") do
-			local current_dir = vim.fn.getcwd()
 			local filename = line:match("[^%s]+$")
-			--command is everything except the filename
 			local command = line:sub(1, #line - #filename)
 			local json = string.format("\t{\n\t\t\"directory\": \"%s\",\n\t\t\"command\": \"%s\",\n\t\t\"file\": \"%s\"\n\t},\n", current_dir, command, filename)
 			file:write(json)
@@ -25,7 +24,7 @@ local function generateCompileCommands()
 		f:close()
 		file:close()
 		vim.cmd('silent! LspRestart')
-		-- print("compile_commands.json generated, LSP restarted")
+		print("compile_commands.json generated, LSP restarted")
 	else
 		print("Make failed")
 	end
